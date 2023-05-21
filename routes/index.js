3cd579d70fbf74f43b2a503ab746df3f81dad626
MJaurigue <jaurigue.michael@gmail.com>
2023-05-20 19:19:46 -0500

    added routes and controllers for signing up and signing in

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 const router = require("express").Router();
 const itemRouter = require("./clothingItems");
-const userRouter = require("./users");
+// const userRouter = require("./users");
+const { login, createUser } = require("../controllers/users");
 
 router.use("/items", itemRouter);
-router.use("/users", userRouter);
+
+// Sign up and sign in routes
+
+router.post("/signup", createUser);
+router.post("/signin", login);
+
+// router.use("/items", itemRouter);
+// router.use("/users", userRouter);
 
 // Use the NOT_FOUND_ERROR variable instead of a string literal
 const { NOT_FOUND_ERROR } = require("../utils/config");
--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,8 @@
 const express = require("express");
 
-const { getUsers, getUser, createUser } = require("../controllers/users");
-
 const { errorHandler } = require("../utils/errors");
 
 const router = express.Router();
-
-// GET all users
-router.get("/", getUsers);
-
-// GET user by ID
-router.get("/:userId", getUser);
-
-// POST new user
-router.post("/", createUser);
 
 // handle errors
 router.use(errorHandler);
