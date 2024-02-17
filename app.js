9743cce5c6aac2f25cdd0e7adc01f67f9bdd24c6
Michael Jaurigue <112568096+Michaeljaurigue@users.noreply.github.com>
2024-02-16 21:35:34 -0600

    Update Cors

--- app.js
+++ app.js
@@ -9,9 +9,9 @@ const { PORT = 3001 } = process.env;
 
 const app = express();
 
-const { errors } = require("celebrate");
-const { requestLogger, errorLogger } = require("./middlewares/logger");
-const errorHandler = require("./middlewares/error-handler");
+const { errors } = require("celebrate"); //celebrate error handling is here for validation errors that are thrown by the celebrate middleware in the routes.
+const { requestLogger, errorLogger } = require("./middlewares/logger"); //importing the requestLogger and errorLogger middlewares from the logger.js file this is for logging the requests and errors to the console.
+const errorHandler = require("./middlewares/error-handler"); //this is the error handler middleware that is used to handle errors that are thrown by the routes.
 
 app.get("/crash-test", () => {
   setTimeout(() => {
@@ -46,9 +46,9 @@ app.use(express.json());
 app.use(
   cors({
     origin: [
-      "http://localhost:3000",
-      "https://wtwrapp.weatherlab.xyz",
-      "https://www.wtwrapp.weatherlab.xyz",
+      "http://localhost:3001",
+      "https://wtwr.devwonders.com",
+      "https://www.wtwr.devwonders.com",
     ],
   })
 );
