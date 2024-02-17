6081a7fa57a4682bd1758d1949211f57ecc069ac
Michael Jaurigue <112568096+Michaeljaurigue@users.noreply.github.com>
2024-02-16 21:48:35 -0600

    Update. Cors

--- app.js
+++ app.js
@@ -9,9 +9,16 @@ const { PORT = 3001 } = process.env;
 
 const app = express();
 
-const { errors } = require("celebrate"); //celebrate error handling is here for validation errors that are thrown by the celebrate middleware in the routes.
-const { requestLogger, errorLogger } = require("./middlewares/logger"); //importing the requestLogger and errorLogger middlewares from the logger.js file this is for logging the requests and errors to the console.
-const errorHandler = require("./middlewares/error-handler"); //this is the error handler middleware that is used to handle errors that are thrown by the routes.
+const { errors } = require("celebrate");
+const { requestLogger, errorLogger } = require("./middlewares/logger");
+const errorHandler = require("./middlewares/error-handler");
+
+// Define allowed origins
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://wtwr.devwonders.com",
+  "https://www.wtwr.devwonders.com",
+];
 
 app.get("/crash-test", () => {
   setTimeout(() => {
@@ -29,31 +36,31 @@ mongoose.connect(process.env.DB_HOST, {
 const db = mongoose.connection;
 
 db.on("error", (err) => {
-  // eslint-disable-next-line no-console
   console.error("Error connecting to db", err);
 });
 db.once("open", () => {
-  // eslint-disable-next-line no-console
   console.log("Connected to the database");
 });
 
 app.use(helmet());
 
+// CORS middleware setup to dynamically allow specified origins
+app.use(cors({
+  origin: function (origin, callback) {
+    // Allow requests with no origin (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      var msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true,
+}));
+
 const routes = require("./routes");
 
 app.use(express.json());
-
-app.use(
-  cors({
-    origin: [
-      "http://localhost:3001",
-      "https://wtwr.devwonders.com",
-      "https://www.wtwr.devwonders.com",
-    ],
-  })
-);
-
-// app.use(cors());
 
 app.use(requestLogger);
 app.use(routes);
@@ -63,9 +70,7 @@ app.use(errors());
 app.use(errorHandler);
 
 app.listen(PORT, () => {
-  // eslint-disable-next-line no-console
   console.log(`App listening on port ${PORT}`);
-  // eslint-disable-next-line no-console
   console.log("Press Ctrl+C to quit.");
 });
 
